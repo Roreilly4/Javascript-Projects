c0958a75f64c6aad6143508a07152acc1579f1ab
Ross O Reilly <80093118+Roreilly4@users.noreply.github.com>
2021-06-23 18:34:49 +0100

    JavaScript Assignment 1

---
+++ Basic Javascript Projects/Project1_expressins_alert/JS1/main.js
@@ -0,0 +1,19 @@
+// Alert box mathod //
+window.alert("Hi, I\'m the alert box!")
+
+// document.write method //
+document.write("Hello World ");
+
+// Assign string values to //
+//two variables and concatenate the variables together //
+// Adding font color //
+
+var A = "I am from the";
+var B = " Emerald Isle";
+var B = B.fontcolor("green");
+document.write(A + B);
+
+//Expressions //
+document.write(23 * 23);
+
+//End Assignment //
