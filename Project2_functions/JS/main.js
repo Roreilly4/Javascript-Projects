f049e829d390a40a2b275569248cf9af1e7a289c
Ross O Reilly <80093118+Roreilly4@users.noreply.github.com>
2021-06-24 20:53:47 +0100

    Added Functions Folder

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+    var x = document.getElementById("demo");
+    x.style.backgroundColor = "green";
+}
+
+function myFunction2() {
+    var y = document.getElementById("demo1");
+    y.style.backgroundColor = "purple";
+}
+
+function myFunction3() {
+    var sentence = "I am learning so";
+    sentence += " much from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
