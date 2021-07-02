10520dc272261e5841df48e7a39a5cac0f41ae4d
Ross O Reilly <80093118+Roreilly4@users.noreply.github.com>
2021-07-02 19:23:26 +0100

    Assignment 3 submission

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,31 @@
+//adding//
+var Simple_Math = 46 + 36;
+document.getElementById("Addition").innerHTML = "46 + 36 =" + Simple_Math;
+//subtraction//
+var Simple_Math = 256 - 674;
+document.getElementById("Subtraction").innerHTML = "256 - 674=" + Simple_Math;
+//random number//
+window.alert(Math.random() * 100);
+//multiply//
+var Simple_Math = 6 * 8;
+document.getElementById("Multiplication").innerHTML = "6 x 8 = " + Simple_Math;
+//divide//
+var Simple_Math = 25 / 6;
+document.getElementById("Division").innerHTML = "25 / 6 = " + Simple_Math;
+//using all //
+var Simple_Math = (45 + 55) * 10 / 2 - 13;
+document.getElementById("All").innerHTML = "45 + 55 multiplied by 10 divided by 2 minus 13 = " + Simple_Math;
+//remainder//
+var simple_Math = 25 % 6;
+document.getElementById("Remainder").innerHTML = "When you divided 25 by 6 you have a remainder of 6" + simple_Math;
+// opposite//
+var x = 10;
+document.getElementById("Math").innerHTML = -x;
+//increment//
+var A = 9;
+A++;
+document.write(A);
+//decrement//
+var B = 7.75;
+B--;
+document.write(B);
