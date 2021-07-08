7042c8dfb420289834d3f6f1d52c067105e3c354
Ross O Reilly <80093118+Roreilly4@users.noreply.github.com>
2021-07-08 21:23:41 +0100

    Added Assignment 5

---
+++ Project5_comparisons_type_coercion/Js/main.js
@@ -0,0 +1,25 @@
+var Employee = {
+    Name: 'John Doe',
+    Age: '35',
+    Employee_Number: 56983,
+    Area: 'Area_2',
+}
+document.write(typeof 'Area'); //type of operator
+
+document.write("10" + 5); //coercion
+
+document.write(57 == 64); //double == operator
+
+C = 54;
+D = '54';
+document.write(C === D); // to check whether the data on the left side of the symbol is equal to the data on the right side
+
+document.write(32 < 12); //lesser than
+
+document.write(32 > 12); //greater than
+
+document.write(10 > 12 && 10 < 10); //AND Operator
+
+document.write(3 > 6 || 1 > 5); //OR Operator
+
+document.getElementById("Not").innerHTML = !(20 > 10); //Not opertator
